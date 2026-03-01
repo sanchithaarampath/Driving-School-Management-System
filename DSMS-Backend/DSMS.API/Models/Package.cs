ee0e0aac3ef99e2bff27f5c430de525425e4e5e3
sanchithaarampath <sanchitha.arampath@gmail.com>
2026-03-01 09:30:00 +0530

    Add Bill and Payment models with instalment tracking and balance fields

---
+++ DSMS-Backend/DSMS.API/Models/Package.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSMS.API.Models;
+
+public partial class Package
+{
+    public int Id { get; set; }
+
+    public string PackageName { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public int TotLectureHours { get; set; }
+
+    public int TotTrainingHours { get; set; }
+
+    public decimal ChargePerExtraHour { get; set; }
+
+    public decimal Rmvcharges { get; set; }
+
+    public decimal DownPaymentAmount { get; set; }
+
+    public decimal Price { get; set; }
+
+    public decimal MaxDiscount { get; set; }
+
+    public bool? Active { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? CreatedDateTime { get; set; }
+
+    public string? LastModifiedBy { get; set; }
+
+    public DateTime? LastModifiedDateTime { get; set; }
+
+    public virtual ICollection<PackageVehicleClass> PackageVehicleClasses { get; set; } = new List<PackageVehicleClass>();
+
+    public virtual ICollection<StudentPackageRegistration> StudentPackageRegistrations { get; set; } = new List<StudentPackageRegistration>();
+}
---
+++ DSMS-Backend/DSMS.API/Models/Payment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSMS.API.Models;
+
+public partial class Payment
+{
+    public int Id { get; set; }
+
+    public int BillId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public DateTime PaymentDate { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string PaymentMethod { get; set; } = null!;
+
+    public string? ReferenceNo { get; set; }
+
+    public string? Remarks { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? CreatedDateTime { get; set; }
+
+    public string? LastModifiedBy { get; set; }
+
+    public DateTime? LastModifiedDateTime { get; set; }
+
+    public virtual Bill Bill { get; set; } = null!;
+
+    public virtual Student Student { get; set; } = null!;
+}
