c4368a09bf85925b0b343f10ebc5e9421f4fe457
sanchithaarampath <sanchitha.arampath@gmail.com>
2026-03-30 09:00:00 +0530

    Add StudentClassProgress and StudentPracticalTestAttempt models

---
+++ DSMS-Backend/DSMS.API/Models/StudentClassProgress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSMS.API.Models;
+
+public partial class StudentClassProgress
+{
+    public int Id { get; set; }
+
+    public int StudentPackageRegistrationId { get; set; }
+
+    public int PackageHeaderId { get; set; }
+
+    public int? InstructorId { get; set; }
+
+    public DateTime? SessionDate { get; set; }
+
+    public decimal? HoursCompleted { get; set; }
+
+    public bool? IsTrialFaced { get; set; }
+
+    public int TrialAttempt { get; set; }
+
+    public int Status { get; set; }
+
+    public bool IsDeclined { get; set; }
+
+    public string? Notes { get; set; }
+
+    public virtual Instructor? Instructor { get; set; }
+
+    public virtual StudentPackageRegistration StudentPackageRegistration { get; set; } = null!;
+
+    public virtual ICollection<StudentPracticalTestAttempt> StudentPracticalTestAttempts { get; set; } = new List<StudentPracticalTestAttempt>();
+}
